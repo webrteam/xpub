4b1d59265859e864722c9855898853960b28e38d
利开团 <likaituan@wecash.net>
2017-05-08 20:42:18 +0800

    quick submit

--- deploy.js
+++ deploy.js
@@ -2,33 +2,42 @@
  * 发版(服务端)
  */
 
-var fs = require("fs");
-var fs2 = require("../core/fs2");
-var ex = require("./ex");
-var start = require("./start");
-var {getArgs} = require("ifun");
+var fs = require('fs');
+var fs2 = require('./fs2');
+var ex = require('./ex');
+var {getArgs, cmd} = require('ifun');
+console.log({cmd: cmd.toString()})
 
-module.exports = function(ua) {
-    var args = getArgs("cmd");
-    args.node = args.node || args.env;
-    args.java = args.java || args.env;
+var doDeploy = function (ua) {
+	var args = getArgs('cmd');
+	args.node = args.node || args.env;
+	args.java = args.java || args.env;
+	var env = args.env || 'test';
 
-    var dir = `${args.dir}/web`;
+	var dir = `${args.dir}/web`;
 
-    //step1 - stop node
-    var pid = ex.getPid(["nobox","deploy", args.port]);
-    pid && ex.kill(pid);
+	//step1 - stop node
+	var pid = ex.getPid(['deploy', args.port]);
+	console.log({pid});
+	pid && ex.kill(pid);
 
-    //step2 - delete old files
-    fs.existsSync(dir) && fs2.rmdir(dir);
+	//step2 - delete old files
+	fs.existsSync(dir) && fs2.rmdir(dir);
 
-    //step3 - add new files
-    fs.mkdirSync(dir);
-    ex.unTar(`${args.dir}/bin.tar.gz`, dir);
+	//step3 - add new files
+	fs.mkdirSync(dir);
+	ex.unTar(`${args.dir}/bin.tar.gz`, dir);
 
-    //step4 - run node
-    args.cmd = "start";
-    args.dir = dir;
-    start(ua, args);
+	//step4 - run node
+	cmd('cp web/package.json package.json', args.dir);
+	cmd('npm run taobao', args.dir);
+	var logFile = `${args.dir}/logs/1.log`;
+	args.cmd = 'start';
+	args.dir = dir;
+	//start(ua, args);
+	console.log({env});
+	cmd(`nohup npm start node=local env=${env} port=8000 > ${logFile} 2>&1 &`, dir);
 
 };
+
+doDeploy();
--- ex.js
+++ ex.js
@@ -1,76 +1,56 @@
-var fs = require("fs");
-var {isFunction} = require("util");
-var {requireJson,cmd,getCmd} = require("ifun");
-
-//获取配置信息
-exports.getConfig = function(args, ua) {
-    var configFile = `${args.dir||ua.path}/config/pub.js`;
-    var config = {
-        pub: requireJson(configFile)
-    };
-    if (isFunction(config)) {
-        config = config(args,ua);
-    }
-    config.args = args;
-    config.ua = ua;
-    return config;
-};
+var os = require('os');
+var {cmd,getCmd} = require('ifun');
 
 //获取进程ID
 //如找不到pid返回0
 exports.getPid = function(keywords){
-    var ps = process.platform=="linux" ? "ps -aux" : "ps aux";
-    var stdout = getCmd(ps);
-
-    var plist = stdout.split('\n');
-    var pid = 0;
-    plist.some(function(line) {
-        var isMatch = keywords.every(function(keyword){
-            //临时兼容
-            if(keyword=="deploy"){
-                return line.includes(keyword) || line.includes("pub_server");
-            }
-            return line.includes(keyword);
-        });
-        if(isMatch) {
-            var _pid = line.trim().split(/\s+/)[1];
-            if(_pid == process.pid){
-                isMatch = false;
-            }else{
-                pid = _pid;
-            }
-        }
-        return isMatch;
-    });
-    return pid;
+	var ps = process.platform=='linux' ? 'ps -aux' : 'ps aux';
+	var stdout = getCmd(ps);
+	var plist = stdout.split('\n');
+	var pid = 0;
+	plist.some(function(line){
+		var isMatch = keywords.every(function(keyword){
+			return line.includes(keyword);
+		});
+		if(isMatch) {
+			var _pid = line.trim().split(/\s+/)[1];
+			if(_pid == process.pid){
+				isMatch = false;
+			}else{
+				pid = _pid;
+			}
+		}
+		return isMatch;
+	});
+	return pid;
 };
 
 //杀进程
 exports.kill = function(pid){
-    cmd(`kill -9 ${pid}`);
+	cmd(`kill -9 ${pid}`);
 };
 
 //解压缩
 exports.unTar = function(tarFile, unTarDir){
-    cmd(`tar -zxf ${tarFile} -C ${unTarDir}/`.replace(/\/\/$/,"/"));
+	cmd(`tar -zxf ${tarFile} -C ${unTarDir}/`.replace(/\/\/$/,'/'));
 };
 
 //获取IP
 exports.getIp = function(){
-    try{
-        var ips = os.networkInterfaces();
-        for(var k in ips) {
-            if(/en|eth/.test(k)) {
-                var a = ips[k];
-                for (var j = 0; j < a.length; j++) {
-                    var o = a[j];
-                    if (o.family == "IPv4" && o.internal === false) {
-                        return o.address;
-                    }
-                }
-            }
-        }
-    }catch(e){
-        return "localhost";
-    }
+	try{
+		var ips = os.networkInterfaces();
+		for(var k in ips) {
+			if(/en|eth/.test(k)) {
+				var a = ips[k];
+				for (var j = 0; j < a.length; j++) {
+					var o = a[j];
+					if (o.family == 'IPv4' && o.internal === false) {
+						return o.address;
+					}
+				}
+			}
+		}
+	}catch(e){
+		return 'localhost';
+	}
 };
