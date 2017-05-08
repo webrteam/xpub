a039daaba7241f6798f6997f6ba5daa7f06271f6
利开团 <likaituan@wecash.net>
2017-05-08 21:28:23 +0800

    quick submit

--- deploy.js
+++ deploy.js
@@ -8,7 +8,6 @@ var fs = require('fs');
 var fs2 = require('./fs2');
 var ex = require('./ex');
 var {getArgs, cmd} = require('ifun');
-console.log({cmd: cmd.toString()})
 
 var doDeploy = function (ua) {
 	var args = getArgs('cmd');
@@ -20,7 +19,6 @@ var doDeploy = function (ua) {
 
 	//step1 - stop node
 	var pid = ex.getPid(['deploy', args.port]);
-	console.log({pid});
 	pid && ex.kill(pid);
 
 	//step2 - delete old files
@@ -37,7 +35,6 @@ var doDeploy = function (ua) {
 	args.cmd = 'start';
 	args.dir = dir;
 	//start(ua, args);
-	console.log({env});
 	cmd(`nohup npm start node=local env=${env} port=8000 > ${logFile} 2>&1 &`, dir);
 
 };
